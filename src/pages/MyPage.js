cc58cd2b298728c95117a9de89849ac0b7a14a7e
Kang-minchul <kmc6381@gmail.com>
2023-10-26 14:53:01 +0900

    여기이따

---
+++ src/pages/MyPage.js
@@ -0,0 +1,11 @@
+function MyPage() {
+    return(
+    <div>
+        <h1>마이 페이지</h1>
+        <button>관심등록리스트</button>
+        <button>회원정보 수정</button>
+    </div>
+    );
+}
+
+export default MyPage;
